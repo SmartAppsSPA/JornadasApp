84eb13187b323336f77ed2a828a49c6d71a25036
Smart Apps Chile <smartapps.chile@gmail.com>
2020-06-25 20:52:43 -0300

    actualizacion de estructura de drawer y rutas

--- App.js
+++ App.js
@@ -1,67 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Text, StyleSheet, StatusBar, ScrollView } from 'react-native';
-import Auth from './src/components/Auth/Auth';
-import firebase from './Firebase/Firebase';
-import Navigation from './src/components/Navigation/Navigation';
-import { NavigationContainer } from '@react-navigation/native';
+import "react-native-gesture-handler";
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  StatusBar,
+  ScrollView,
+  View,
+} from "react-native";
+import Auth from "./src/components/Auth/Auth";
+import firebase from "./Firebase/Firebase";
+import MyDrawer from "./src/MyDrawer";
 
 export default function App() {
   const [user, setUser] = useState(undefined);
-  const [fbUserData, setFbUserData] = useState(null);
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((response) => {
-      setUser(response)
-      if (response) {
-        firebase.database().ref(`Users/${response.uid}` || `Company/${response.uid}`).on('value', snapshot => {
-          setFbUserData(snapshot.val());
-        });
-      }
-
+      setUser(response);
     });
   }, []);
 
   if (user === undefined) return null;
-
+  console.log(user);
   return (
     <>
-      {user ?
-        <>
-          < NavigationContainer >
-            <StatusBar />
-            <Navigation />
-          </NavigationContainer>
-        </>
-        :
+      {user ? (
+        <MyDrawer />
+      ) : (
         <>
           <StatusBar />
           <SafeAreaView style={styles.background}>
             <ScrollView>
               <Auth />
             </ScrollView>
-            <Text style={styles.smartApps}>
-              ©2020 Powered by Smartapps
-            </Text>
+            <Text style={styles.smartApps}>©2020 Powered by Smartapps</Text>
           </SafeAreaView>
         </>
-      }
+      )}
     </>
   );
 }
 
-
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: '#03255F',
-    height: '100%',
-    alignItems: 'center',
+    backgroundColor: "#03255F",
+    height: "100%",
+    alignItems: "center",
   },
   button: {
     width: 300,
     height: 50,
-    backgroundColor: '#F5C300',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F5C300",
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 10,
     marginBottom: 10,
     borderRadius: 8,
@@ -69,9 +61,8 @@ const styles = StyleSheet.create({
   },
   smartApps: {
     fontSize: 12,
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     marginBottom: 50,
   },
 });
-
